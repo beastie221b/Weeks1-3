5c81e8664a158e6dd7485fcb3be0e867bb809397
beastie221b <zhouyiwe@sheridancollege.ca>
2025-01-31 21:45:28 -0500

    made some rising bubbles.

---
+++ Assets/Mechanical Drawing/Scripts/risingBubble.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class risingBubble : MonoBehaviour
+{
+    // Initialization
+    public AnimationCurve curve;
+    public float speed = 0.05f;
+    [Range(0, 10)]
+    public float t;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update the bubble's size and position based on time
+        t += Time.deltaTime;
+        transform.localScale = Vector2.one * curve.Evaluate(t);
+        Vector3 bubblePosition = transform.position;
+        bubblePosition.y += speed;
+        transform.position = bubblePosition;
+    }
+}
